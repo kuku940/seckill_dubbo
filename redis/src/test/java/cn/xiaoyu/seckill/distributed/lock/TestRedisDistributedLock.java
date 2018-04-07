f31e051d4846695c4905c7b1fb5f4a95942ef2f3
章小雨 <roingeek@qq.com>
2018-04-07 14:37:50 +0800

    添加Redis分布式锁及测试用例

---
+++ redis/src/test/java/cn/xiaoyu/seckill/distributed/lock/TestRedisDistributedLock.java
@@ -0,0 +1,60 @@
+package cn.xiaoyu.seckill.distributed.lock;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Redis创建分布式锁的测试用例
+ *
+ * @author Roin_zhang
+ * @date 2018/4/7 14:20
+ */
+public class TestRedisDistributedLock {
+    public static void main(String[] args) {
+        SKService service = new SKService();
+        for (int i = 0; i < 50; i++) {
+            SeckillThread thread = new SeckillThread(service);
+            thread.start();
+        }
+    }
+}
+
+class SeckillThread extends Thread {
+    private SKService service;
+
+    public SeckillThread(SKService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.seckill();
+    }
+}
+
+class SKService {
+    private static JedisPool pool = null;
+    private RedisDistributedLock lock = new RedisDistributedLock(pool);
+    int n = 500;
+
+    static {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(200);    // 设置最大连接数
+        config.setMaxIdle(8);   // 设置最大空闲数
+        config.setMaxWaitMillis(1000 * 100); // 设置最大等待时间为100秒
+
+        // 在borrow一个jedis实例时，是否需要验证，若为true，则所有的jedis实例均是可用的
+        config.setTestOnBorrow(true);
+        pool = new JedisPool(config, "127.0.0.1", 6379, 3000);
+    }
+
+    public void seckill() {
+        // 返回锁的value，供释放锁的时候进行判断
+        String identifier = lock.lockWithTimeout("resource", 5000, 1000);
+        System.out.println(Thread.currentThread().getName() + " get lock!");
+        System.out.println(--n);
+        lock.releaseLock("resource", identifier);
+        System.out.println(Thread.currentThread().getName() + " release lock!");
+        System.out.println("-----");
+    }
+}
