1766e8e7f2f5786337c9fa1203f560a7b2dadf6c
章小雨 <roingeek@qq.com>
2018-04-09 10:52:40 +0800

    为maven打包工具添加排除的文件夹的配置

---
+++ api/src/main/java/cn/xiaoyu/seckill/xxxx/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.xiaoyu.seckill.xxxx;
+
+/**
+ * 此包用来测试maven打包排除指定文件夹
+ * @author Roin_zhang
+ * @date 2018/4/9 10:38
+ */
+public class HelloWorld {
+    public void say(){
+        System.out.println("hello world!");
+    }
+}
